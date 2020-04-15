204c2e82d0fcf79e284895179b181583de81f95b
Kamil Samigullin <kamil@samigullin.info>
2020-04-15 23:15:12 +0300

    update tools, remove vendor

--- tools/tools.go
+++ tools/tools.go
@@ -11,7 +11,7 @@ import (
 	_ "golang.org/x/tools/cmd/goimports"
 )
 
-//go:generate go build -v -o=${ROOT}bin/mockgen github.com/golang/mock/mockgen
-//go:generate go build -v -o=${ROOT}bin/golangci-lint github.com/golangci/golangci-lint/cmd/golangci-lint
-//go:generate go build -v -o=${ROOT}bin/egg github.com/kamilsk/egg
-//go:generate go build -v -o=${ROOT}bin/goimports golang.org/x/tools/cmd/goimports
+//go:generate go install github.com/golang/mock/mockgen
+//go:generate go install github.com/golangci/golangci-lint/cmd/golangci-lint
+//go:generate go install github.com/kamilsk/egg
+//go:generate go install golang.org/x/tools/cmd/goimports
